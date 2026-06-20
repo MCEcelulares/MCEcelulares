25374f66cb1c98127ea4800d2c07e8c7a2902fe3
Raf0627 <rafaelfrossard076@gmail.com>
2026-06-20 14:52:31 -0300

    test: finalizando testes de cadastro de usuário

--- tests/signup.spec.ts
+++ tests/signup.spec.ts
@@ -1,22 +1,23 @@
 import { test } from "@playwright/test";
-import SignupPage from "./page-objects/Signuppage";
+import SignupPage from "./page-objects/SignupPage";
 
 test.describe("Página de Cadastro", () => {
 
     test("Deve realizar o cadastro com sucesso", async ({ page }) => {
         const signupPage = new SignupPage(page);
+        const uniqueEmail = `rafael+${Date.now()}@gmail.com`;
 
         await signupPage.visit();
-        
+
         await signupPage.signup(
             "Rafael",
-            "rafaelfrossard123@gmail.com", 
-            "123.123.123-12", 
-            "(12) 12345-1234", 
-            "@Rafael1", 
+            uniqueEmail,
+            "123.123.123-12",
+            "(12) 12345-1234",
+            "@Rafael1",
             "@Rafael1"
         );
-        
+
         await signupPage.verifySuccessMessage();
     });
 
@@ -24,16 +25,16 @@ test.describe("Página de Cadastro", () => {
         const signupPage = new SignupPage(page);
 
         await signupPage.visit();
-        
+
         await signupPage.signup(
             "Rafael",
-            "rafael.duplicado@gmail.com", 
-            "123.123.123-12", 
-            "(12) 12345-1234", 
-            "@Rafael1", 
+            "rafaelfrossard@gmail.com",
+            "123.123.123-12",
+            "(12) 12345-1234",
+            "@Rafael1",
             "@Rafael1"
         );
-        
+
         await signupPage.verifyErrorMessage();
     });
 
@@ -41,16 +42,16 @@ test.describe("Página de Cadastro", () => {
         const signupPage = new SignupPage(page);
 
         await signupPage.visit();
-        
+
         await signupPage.signup(
             "Rafael",
-            "rafaelfrossard@gmail.com", 
-            "123.123.123-12", 
-            "(12) 12345-1234", 
-            "@Rafael1", 
+            "rafaelfrossard@gmail.com",
+            "123.123.123-12",
+            "(12) 12345-1234",
+            "@Rafael1",
             "SenhaDiferente123"
         );
-        
+
         await signupPage.verifyErrorMessage();
     });
 });
